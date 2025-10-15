1b482671dfe0a35cb51495e91be323794778c4d4
gpala39 <gpala39@wgu.edu>
2025-10-15 16:52:07 -0500

    Added major axis bending calculation.

---
+++ major.py
@@ -0,0 +1,74 @@
+#Program for the evaluation and design of a steel plate in major axis bending
+#AISC Manual of Steel Construction (ASD), 14th Ed, 2011
+
+# Geometric Input
+# t = float(input("What is the plate thickness (in.)? "))
+# print("The plate thickness is: t = " + str(t) + " in.")
+# d = float(input("What is the plate depth (in.)? "))
+# print("The plate depth is: d = " + str(d) + " in.")
+# Lb = float(input("What is the plate length (in.)? "))
+# print("The plate length is: Lb = " + str(Lb) + " in.")
+# e = float(input("What is the eccentricity of the load from bottom of plate (in.)? "))
+# print("The eccentricity is: e = " + str(e) + " in.")
+# P_max = float(input("What is the maximum applied lateral load (kip)? "))
+# print("The maximum applied lateral load is: P_max = " + str(P_max) + " kip")
+#
+# # Constants
+# # Yield Strength (ksi)
+# Fy = float(36)
+# # Modulus of Elasticity (ksi)
+# E = float(29000)
+# # Lateral-torsional buckling modification factor
+# Cb = float(1)
+#
+# # Modulus Calculation
+# print()
+# Z = float(((t*d*d))/4)
+# print("The plastic section modulus is: Z = " + str(Z.__round__(2)) + " in.^3")
+# S = float(((t*d*d))/6)
+# print("The elastic section modulus is: S = " + str(S.__round__(2)) + " in.^3")
+#
+#
+# print()
+# print("The plate will be analyzed for major axis bending.")
+# # Check yielding for rectangular bars bent about minor axis (AISC F11-1)
+# print()
+# Lbd_t2 = float(Lb * d)/(t*t)
+# E08_Fy = float((0.08*E)/Fy)
+# E19_Fy = float((1.9*E)/Fy)
+# My = float(Fy * S)/12
+# Mp = float(Fy * Z)/12
+# if Lbd_t2 <= E08_Fy:
+#     My_1_6 = float((1.6 * Fy * S)/12)
+#     print("1.6My = " + str(My_1_6) + " k-ft")
+#     Mp = float((Z * Fy)/12)
+#     print("Mp = " + str(Mp.__round__(2)) + " k-ft")
+#     print()
+#     print("Nominal Moment = Minimum of 1.6My and Mp:")
+#     Mn = min(Mp, My_1_6)
+#     print("Mn = " + str(Mn.__round__(2)) + " k-ft")
+#
+# # Check lateral torsional buckling (AISC F11-2)
+# else:
+#     if Lbd_t2 <= E19_Fy and E08_Fy < Lbd_t2:
+#         Mn = min(float(Cb*(1.52 - 0.274 * (Lbd_t2) * (Fy / E)) * My), Mp)
+#     else:
+#         if E19_Fy < Lbd_t2:
+#             Fcr = float((1.9*E*Cb)/Lbd_t2)
+#             Mn = min((Fcr * S), Mp)
+#     print("Mn = " + str(Mn.__round__(2)) + " k-ft")
+#
+# # Determine maximum allowable applied load
+# print()
+# P_all = ((Mn/1.67)/(e/12))
+# print("Maximum allowable lateral load: P_all = " + str(P_all.__round__(2)) + " kip")
+#
+# # Bending Strength Unity Check
+# print()
+# Unity = P_max/P_all
+# print("Bending Unity Strength = P_max/P_all = " + str(Unity.__round__(2)))
+#
+# if Unity <= 1:
+#     print("Pass")
+# else:
+#     print("Fail")
--- minor.py
+++ minor.py
@@ -1,57 +1,56 @@
 # Program for the evaluation and design of a steel plate in minor axis bending
 # AISC Manual of Steel Construction (ASD), 14th Ed, 2011
-
+#
 # Geometric Input
-t = float(input("What is the plate thickness (in.)? "))
-print("The plate thickness is: t = " + str(t) + " in.")
-d = float(input("What is the plate depth (in.)? "))
-print("The plate depth is: d = " + str(d) + " in.")
-Lb = float(input("What is the plate length (in.)? "))
-print("The plate length is: Lb = " + str(Lb) + " in.")
-e = float(input("What is the eccentricity of the load from bottom of plate (in.)? "))
-print("The eccentricity is: e = " + str(e) + " in.")
-P_max = float(input("What is the maximum applied lateral load (kip)? "))
-print("The maximum applied lateral load is: P_max = " + str(P_max) + " kip")
-
-# Constants
-# Yield Strength (ksi)
-Fy = float(36)
-# Modulus of Elasticity (ksi)
-E = float(29000)
-# Lateral-torsional buckling modification factor
-Cb = float(1)
-
-
-# Modulus Calculation
-print()
-Z = float(((d*t*t))/4)
-print("The plastic section modulus is: Z = " + str(Z.__round__(2)) + " in.^3")
-S = float(((d*t*t))/6)
-print("The elastic section modulus is: S = " + str(S.__round__(2)) + " in.^3")
-
-# Check yielding for rectangular bars bent about minor axis (AISC F11-1)
-print()
-My_1_6 = float((1.6 * Fy * S)/12)
-print("1.6My = " + str(My_1_6) + " k-ft")
-Mp = float((Z * Fy)/12)
-print("Mp = " + str(Mp.__round__(2)) + " k-ft")
-print()
-print("Nominal Moment = Minimum of 1.6My and Mp:")
-Mn = min(Mp, My_1_6)
-print("Mn = " + str(Mn.__round__(2)) + " k-ft")
-
-# Determine maximum allowable applied load
-print()
-P_all = ((Mn/1.67)/(e/12))
-print("Maximum allowable lateral load: P_all = " + str(P_all.__round__(2)) + " kip")
-
-# Bending Strength Unity Check
-print()
-Unity = P_max/P_all
-print("Bending Unity Strength = P_max/P_all = " + str(Unity.__round__(2)))
-
-if Unity <= 1:
-    print("Pass")
-else:
-    print("Fail")
-
+# t = float(input("What is the plate thickness (in.)? "))
+# print("The plate thickness is: t = " + str(t) + " in.")
+# d = float(input("What is the plate depth (in.)? "))
+# print("The plate depth is: d = " + str(d) + " in.")
+# Lb = float(input("What is the plate length (in.)? "))
+# print("The plate length is: Lb = " + str(Lb) + " in.")
+# e = float(input("What is the eccentricity of the load from bottom of plate (in.)? "))
+# print("The eccentricity is: e = " + str(e) + " in.")
+# P_max = float(input("What is the maximum applied lateral load (kip)? "))
+# print("The maximum applied lateral load is: P_max = " + str(P_max) + " kip")
+#
+# # Constants
+# # Yield Strength (ksi)
+# Fy = float(36)
+# # Modulus of Elasticity (ksi)
+# E = float(29000)
+# # Lateral-torsional buckling modification factor
+# Cb = float(1)
+#
+#
+# # Modulus Calculation
+# print()
+# Z = float(((d*t*t))/4)
+# print("The plastic section modulus is: Z = " + str(Z.__round__(2)) + " in.^3")
+# S = float(((d*t*t))/6)
+# print("The elastic section modulus is: S = " + str(S.__round__(2)) + " in.^3")
+#
+# # Check yielding for rectangular bars bent about minor axis (AISC F11-1)
+# print()
+# My_1_6 = float((1.6 * Fy * S) / 12)
+# print("1.6My = " + str(My_1_6.__round__(2)) + " k-ft")
+# Mp = float((Z * Fy) / 12)
+# print("Mp = " + str(Mp.__round__(2)) + " k-ft")
+# print()
+# print("Nominal Moment = Minimum of 1.6My and Mp:")
+# Mn = min(Mp, My_1_6)
+# print("Mn = " + str(Mn.__round__(2)) + " k-ft")
+#
+# # Determine maximum allowable applied load
+# print()
+# P_all = ((Mn / 1.67) / (e / 12))
+# print("Maximum allowable lateral load: P_all = " + str(P_all.__round__(2)) + " kip")
+#
+# # Bending Strength Unity Check
+# print()
+# Unity = P_max / P_all
+# print("Bending Unity Strength = P_max/P_all = " + str(Unity.__round__(2)))
+#
+# if Unity <= 1:
+#     print("Pass")
+# else:
+#     print("Fail")
